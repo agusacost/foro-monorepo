0d2fbc6a780bf19ca6ed0a65820eae8ab53d7a92
Agustin Acosta <juanagustinacost@gmail.com>
2025-04-07 00:10:26 -0300

    enable cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,9 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // importante para preflight
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
 
 //send api-v1 branch to remote
